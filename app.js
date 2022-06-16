9fa7689ad6a8e3167dfece761df4c33c9f578d41
0utrunner <105307161+0utrunner@users.noreply.github.com>
2022-06-15 23:19:17 -0500

    Full game

--- app.js
+++ app.js
@@ -1,2 +1,39 @@
-console.log("HELLO PAPA PLATOON!")
-// Your function(s) should go here that will interact with the webpage or DOM
+console.log("HELLO ROMEO PLATOON!")
+
+secretNumber = Math.floor(Math.random() * 19) + 1
+guesses = 0
+
+function guessingGame(){
+    
+    if(guesses == 6){
+        document.getElementById("hint").innerHTML = "No more chances! Sea ya later! (✧∀✧)"
+        document.getElementById("chances").innerHTML--
+        setTimeout(function(){
+            window.location.reload(1);
+        }, 5000);
+        let element = document.getElementById("game-over")
+        element.classList.add("water")
+    }
+    else if(document.getElementById("answer").value > 20){
+        guesses++
+        document.getElementById("hint").innerHTML = 'Wasting your guesses, huh? ( ｡•́‿•̀｡)'
+        document.getElementById("chances").innerHTML--
+    }
+    else if(document.getElementById("answer").value < secretNumber){
+        guesses++
+        document.getElementById("hint").innerHTML = 'Too low!! :D'
+        document.getElementById("chances").innerHTML--
+    }
+    else if(document.getElementById("answer").value > secretNumber){
+        guesses++
+        document.getElementById("hint").innerHTML = 'Hey! Too high! >:o'
+        document.getElementById("chances").innerHTML--
+    }
+    else if(document.getElementById("answer").value == secretNumber){
+        document.getElementById("hint").innerHTML = "Huh? That's the answer ._."
+        setTimeout(function(){
+            window.location.reload(1);
+        }, 5000);
+    }
+
+}
